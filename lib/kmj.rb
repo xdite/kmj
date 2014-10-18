4ee48a0df6d934e481cb31c1648d272887fd77e8
xdite <xuite.joke@gmail.com>
2014-10-18 21:38:49 +0800

    fix typo

--- lib/kmj.rb
+++ lib/kmj.rb
@@ -8,18 +8,20 @@ module Kmj
       @url = url
     end
     def parse
-      list = Nokogiri::HTML(open(url))
+      list = Nokogiri::HTML(open(@url))
       list = list.css('div.vpl')
       File.open("result.txt", "w") do |file|
         list.css("a").each do |a|
           link = a.attr('href')
           if link.include?("ed2k://") || link.include?("thunder://")
             file.puts link
+            puts link
           end
         end
       end
 
       system "pbcopy < result.txt"
+      puts "Links copied to clipboard!"
     end
 
 
